132dd454ceb508bdbb368a698e8d2cb9f21fa90f
Agustin Juarez <agustin.juarez8483@gmail.com>
2023-06-10 14:27:24 -0300

    v0.2

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Tic-tok-web
+
+Ir a la carpeta my-app para instalar modulos de react
--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes, } from 'react-router-dom';
+
+// Importa tus componentes personalizados
+/* import Home from './components/Home';
+import About from './components/About';
+import NotFound from './components/NotFound'; */
+
+function Home () {
+  return (
+    <div>Home</div>
+  )
+}
+  
+function About () {
+  return <div>About</div>
+}
+
+function NotFound () {
+  return <div>Not Found</div>
+}
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    <Router>
+      <Routes>
+        <Route path="/about" element={<About/>} />
+        <Route path="/" element={<Home/>} />
+        <Route path="*" element={<NotFound/>} />
+      </Routes>
+    </Router>
+  )
 }
 
 export default App;
