33a768272ac6ae128f6f273d5946abc2f418f2e6
Agustin Juarez <agustin.juarez8483@gmail.com>
2023-07-02 11:19:02 -0300

    0.4

--- README.md
+++
@@ -1,3 +0,0 @@
-# Tic-tok-web
-
-Ir a la carpeta my-app para instalar modulos de react
--- my-app/src/Pages/Upload.js
+++ my-app/src/Pages/Upload.js
@@ -1,3 +1,31 @@
+import axios from 'axios';
+import React, { useCallback } from 'react';
+import { useNavigate } from "react-router-dom";
+
 export function Upload () {
-    return <div>Upload</div>
+
+  const navigate = useNavigate();
+
+  const activeBackend = useCallback((servidorActivo) => {
+    console.log('hola');
+    console.log(servidorActivo);
+    navigate('/');
+  
+  }, [])
+
+  const uploadClick = useCallback(() => {
+    axios.get('http://localhost:3000/')
+      .then(response => {
+        activeBackend(response.data);
+      })
+      .catch(error => {
+        // Manejo de errores
+        console.error(error);
+      })
+  }, []);
+
+    return <div>
+            <p>upload</p>
+            <button onClick={uploadClick}>Hacer clic</button>
+          </div>
   }
